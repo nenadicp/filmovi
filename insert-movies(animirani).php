d81b3f3f80f6a48aed386b26a7a0186249c750fa
nenadicp <petar.nenadi@gmail.com>
2018-05-29 11:16:28 +0200

    29/05/2018

--- insert-movies(animirani).php
+++ insert-movies(animirani).php
@@ -30,6 +30,10 @@
 <html lang="en">
 <head>
     <title>Unos filmova - ANIMIRANI</title>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script type="text/javascript" src="javascript/jquery.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
         body {
             background-color: #b3ffff;
--- insert-movies(filmovi).php
+++ insert-movies(filmovi).php
@@ -30,6 +30,10 @@
 <html lang="en">
 <head>
     <title>Unos filmova - FILMOVI</title>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script type="text/javascript" src="javascript/jquery.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
     <p>
