d0a9286400989fc97ae67d828b64a06c07c5d688
nenadicp <petar.nenadi@gmail.com>
2018-06-05 14:15:01 +0200

    05/06/2018

--- login.php
+++ login.php
@@ -28,6 +28,31 @@
     <meta charset="UTF-8">
     <title>Prijava</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <style>
+        body{
+            background-color: #b3d9ff;
+        }
+
+        table {
+            padding: 25px; 
+            background-color: #ffff99;
+            border-radius: 6px;
+            height: auto;
+            width: 600px;
+            font-size: 19px;
+            box-shadow: 5px 5px #737373;
+        }
+
+        .unos {
+            height: 25px;
+            font-size: 17px;
+        }
+
+        .button {
+            height: 25px;
+            width: 90px;
+        }
+    </style>
 </head>
 <body>
     <?php
@@ -38,21 +63,38 @@
     ?>
 
     <form method="post" action="login.php">
-        <table>
+        <p style="padding-top: 170px;"></p>
+        <table align="center">
             <tr>
                 <td>Korisničko ime:</td>
-                <td><input type="text" name="username"></td>
+                <td><input class="unos" type="text" name="username" placeholder="Unesite korisničko ime"></td>
             </tr>
+            <tr style="height: 20px;"></tr>
+            <tr></tr>
             <tr>
                 <td>Lozinka:</td>
-                <td><input type="password" name="password"></td>
+                <td><input class="unos" type="password" name="password" id="password" placeholder="Unesite lozinku">
+                &nbsp;&nbsp;
+                <input type="checkbox" onclick="myFunction()">Prikaži lozinku</td>
             </tr>
+            <tr style="height: 20px;"></tr>
+            <tr></tr>
             <tr>
                 <td></td>
-                <td><input type="submit" name="login_btn" value="Prijavi se"></td>
+                <td><input class="button" type="submit" name="login_btn" value="Prijavi se"></td>
             </tr>
         </table>
     </form>
 
+    <script>
+        function myFunction(){
+            var x = document.getElementById("password");
+            if(x.type === "password") {
+                x.type = "text";
+            } else {
+                x.type = "password";
+            }
+        }
+    </script>
 </body>
 </html>
